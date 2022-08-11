a5f21b3b520702d8aab4a807e1011d0e2b19f20b
Luiyi_FHB <luiyifernando@gmail.com>
2022-08-11 11:14:31 -0500

    update header file with librarys

--- shell.h
+++ shell.h
@@ -1,6 +1,20 @@
-#ifndef SHELL_H_
-#define SHELL_H_
+#ifndef _SHELL_H_
+#define _SHELL_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stddef.h>
+#include <signal.h>
+#include <string.h>
+#include <fcntl.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
+int main (void);
 int hell(void);
 
-#endif
+#endif /* _SHELL_H_ */
