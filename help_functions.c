6a84a1d631636fd30e080d6bca6e4426ce89851e
Daniela1026 <cartagenad49@gmail.com>
2022-08-13 11:22:17 -0700

    UNIX update

--- help_functions.c
+++ help_functions.c
@@ -61,28 +61,31 @@ int execute(char **args)
 {
 	pid_t my_pid;
 	int status;
-
-	if (strncmp("exit", args[0], 4) == 0)
-		return (-1);
-
+	char **envp;
+	
+	envp = environ;
+	
 	my_pid = fork();
-
-	if (my_pid == -1)
+	
+	if (my_pid == 0)
 	{
-		perror("Error");
-		return (1);
+		execve(args[0], args, envp);
+		perror("ERROR");
+		return (-1);
 	}
-	else if (my_pid == 0)
+	else if (my_pid > 0)
 	{
-		if (execve(args[0], args, NULL) == -1)
+		do
 		{
-			perror("Error");
-			exit(-1);
+			waitpid(my_pid, &status, WUNTRACED);
 		}
+		while (!WIFEXITED(status) && !WIFSIGNALED(status));
 	}
 	else
-		wait(&status);
-
+	{
+		perror("hsh");
+		return (-1);
+	}
 	return (0);
 }
 
@@ -121,4 +124,66 @@ char** token_generate(char *line_read, ssize_t num)
     }
     tokens[i] = NULL;
     return (tokens);
+}
+
+void builtin_cmd(char **args)
+{
+	int num = 0;
+	int i;
+
+	struct builtin builtins[] = {
+        {"exit", h_exit},
+        {"cd", h_cd},
+        {"help", h_help},
+        {"env", h_env},
+    };
+	num = sizeof(builtins) / sizeof(struct builtin);
+
+    for (i = 0; i < num; i++)
+    {
+        if (strcmp(args[0], builtins[i].name) == 0)
+	{
+            builtins[i].func(args);
+            return;
+        }
+    }
+}
+
+void h_exit(char **args)
+{
+    (void)args;
+    exit(0);
+}
+
+void h_cd(char **args)
+{
+    if (args[1] == NULL) {
+        fprintf(stderr, "hsh: cd: missing argument\n");
+    } else {
+        if (chdir(args[1]) != 0) {
+            perror("hsh: cd");
+        }
+    }
+}
+
+void h_help(char **args)
+{
+    char *helptext =
+        "The following commands are available:\n"
+        "  cd       Change the working directory.\n"
+        "  exit     Exit the shell.\n"
+        "  help     Print this help text.\n";
+    printf("%s", helptext);
+
+    (void)args;
+}
+
+void h_env(char **args)
+{
+    char **env = environ;
+
+    while (*env)
+      printf("%s\n", *env++);
+    
+    (void)args;
 }
--- shell.h
+++ shell.h
@@ -15,11 +15,23 @@
 #include <sys/stat.h>
 #include <sys/wait.h>
 
+extern char **environ;
+
+struct builtin {
+    char *name;
+    void (*func)(char **args);
+};
+
 int _args(void);
 int main(void);
 char *read_line(ssize_t *line);
 char **split_line(char *line);
 int execute(char **args);
 char** token_generate(char *line_read, ssize_t num);
+void builtin_cmd(char **args);
+void h_exit(char **args);
+void h_cd(char **args);
+void h_help(char **args);
+void h_env(char **args);
 
 #endif /* _SHELL_H_ */
