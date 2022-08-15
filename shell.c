43a329b959313a18928246c481046642ac5e8fe4
Luiyi_FHB <luiyifernando@gmail.com>
2022-08-15 18:30:06 -0500

    fixed segmentation fault in no-interactive mode

--- shell.c
+++ shell.c
@@ -15,28 +15,22 @@ int main(void)
 	while (true)
 	{
 		if (isatty(STDIN_FILENO))
-		{
 			write(STDOUT_FILENO, "$ ", 2);
-			line = read_line(&num_read);
-			tokens = token_generate(line, num_read);
-			execute(tokens);
-			free(line);	
-			free(tokens);
 
-			if (num_read == EOF)
-			{
-				write(2, "Exiting shell....\n", 18);
-				exit(0);
-			}
-			if (num_read == 1)
-				continue;
-		}
-		else
+		line = read_line(&num_read);
+		if (num_read == EOF)
 		{
-			line = read_line(&num_read);
-			write(STDOUT_FILENO, line, num_read);
-			break;
+/*			write(2, "Exiting shell....\n", 18);*/
+			exit(EXIT_SUCCESS);
 		}
+
+		if (num_read == 1)
+			continue;
+
+		tokens = token_generate(line, num_read);
+		execute(tokens);
+		free(line);	
+		free(tokens);
 	}
 	return (EXIT_SUCCESS);
 }
