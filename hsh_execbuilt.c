c7a92dabf8ff75ea5137a21108cbf357bf3de151
Daniela1026 <cartagenad49@gmail.com>
2022-08-18 21:45:52 -0700

    update

--- hsh_execbuilt.c
+++ hsh_execbuilt.c
@@ -8,19 +8,19 @@
  */
 char *read_line(char *line)
 {
-	int size = 0;
-	size_t buflen = 0;
-
-	if (getline(&line, &buflen, stdin) == EOF)
-	{
-		free(line);
-		exit(EXIT_SUCCESS);
-
-	}
-	size = _strlen(line);
-	line[size - 1] = '\0';
-
-	return (line);
+         int size = 0;
+         size_t buflen = 0;
+ 
+         if (getline(&line, &buflen, stdin) == EOF)
+         {
+                 free(line);
+                 exit(EXIT_SUCCESS);
+ 
+         }
+         size = _strlen(line);
+         line[size - 1] = '\0';
+ 
+         return (line);
 }
 
 /**
@@ -33,20 +33,20 @@ char *read_line(char *line)
  */
 void token_generate(char **args, char *line, char *delim)
 {
-	ssize_t i = 0;
-	char *arg = NULL;
+         ssize_t i = 0;
+         char *arg = NULL;
 
-	arg = strtok(line, delim);
-	while (arg != NULL)
-	{
-		args[i] = arg;
-		i++;
-		arg = strtok(NULL, delim);
-	}
+         arg = strtok(line, delim);
+         while (arg != NULL)
+         {
+                 args[i] = arg;
+                 i++;
+                 arg = strtok(NULL, delim);
+         }
 
-	args[i] = NULL;
+         args[i] = NULL;
 }
-
+ 
 /**
  *execute - execute new process
  *@args: argumens input
@@ -55,85 +55,80 @@ void token_generate(char **args, char *line, char *delim)
  */
 int execute(char **args)
 {
-	char *command = NULL;
-	pid_t my_pid;
-	int status = 0;
+        char *command = NULL;
+        pid_t my_pid;
+        int status = 0;
 
-	if (!args[0])
-		return (ARG_ONLY_SPACE);
+         if (!args[0])
+              return (ARG_ONLY_SPACE);
+         if (builtin_cmd(args) == EXIT_SUCCESS)
+                 return (EXIT_SUCCESS);
 
+          command = cmd(args[0]);
+         if (!command)
+                 return (EXIT_FAILURE);
 
-	if (builtin_cmd(args) == EXIT_SUCCESS)
-		return (EXIT_SUCCESS);
+         my_pid = fork();
 
-	command = cmd(args[0]);
-	if (!command)
-		return (EXIT_FAILURE);
+         if (my_pid == 0)
+         {
+                 execve(command, args, environ);
+                 exit(EXIT_SUCCESS);
+         }
+        else if (my_pid > 0)
+          {
+                  do {waitpid(my_pid, &status, WUNTRACED);
+                  } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+                  if (_strcmp(command, args[0]))
+                          free(command);
+          }
 
-	my_pid = fork();
-
-	if (my_pid == 0)
-	{
-		execve(command, args, environ);
-		exit(EXIT_SUCCESS);
-	}
-	else if (my_pid > 0)
-	{
-		do {waitpid(my_pid, &status, WUNTRACED);
-		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
-		if (_strcmp(command, args[0]))
-			free(command);
-	}
-
-	else
-		perror("hsh");
-
-	return (status);
+         return (status);
 }
 
 /**
- *cmd - parse command
+ *_which - parse command
  *@command: input command
  *
  *Return: NULL
  */
 char *cmd(char *command)
 {
-	char *path = getenv("PATH"), *copy_path = NULL;
-	char *path_args = NULL, *dir = NULL;
-	int command_len = _strlen(command), dir_len = 0;
-	struct stat testfile;
+         char *path = getenv("PATH"), *copy_path = NULL;
+         char *path_args = NULL, *dir = NULL;
+         int command_len = _strlen(command), dir_len = 0;
+         struct stat testfile;
 
-	if (stat(command, &testfile) == 0)
-		return (command);
+          if (stat(command, &testfile) == 0)
+                 return (command);
 
-	if (path)
-	{
-		copy_path = _strdup(path);
-		if (!copy_path)
-			return (NULL);
+         if (path)
+         {
+                 copy_path = _strdup(path);
+                 if (!copy_path)
+                         return (NULL);
 
-		path_args = strtok(copy_path, ":");
-		while (path_args != NULL)
-		{
-			dir_len = _strlen(path_args) + command_len + 1;
-			dir = malloc(sizeof(char) * dir_len + 1);
-			if (!dir)
-				return (NULL);
-			_strcpy(dir, path_args);
-			_strcat(dir, "/");
-			_strcat(dir, command);
-			_strcat(dir, "\0");
-			if (stat(dir, &testfile) == 0)
-			{
-				free(copy_path);
-				return (dir);
-			}
-			free(dir);
-			path_args = strtok(NULL, ":");
-		}
-		free(copy_path);
-		return (NULL);
-	}
-	return (NULL);
-}
+                 path_args = strtok(copy_path, ":");
+                 while (path_args != NULL)
+                 {
+                         dir_len = _strlen(path_args) + command_len + 1;
+                          dir = malloc(sizeof(char) * dir_len + 1);
+                          if (!dir)
+                                  return (NULL);
+                         _strcpy(dir, path_args);
+                         _strcat(dir, "/");
+                         _strcat(dir, command);
+                         _strcat(dir, "\0");
+                         if (stat(dir, &testfile) == 0)
+                         {
+                                free(copy_path);
+                                return (dir);
+                        }
+                        free(dir);
+                       path_args = strtok(NULL, ":");
+                 }
+                free(copy_path);
+                return (NULL);
+        }
+        return (NULL);
+}                                                                            
