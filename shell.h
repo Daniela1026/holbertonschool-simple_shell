fbe95ae7cfcff84c7756ca12e1092f9fb2a8cd95
Daniela1026 <cartagenad49@gmail.com>
2022-08-18 21:10:53 -0700

    Update Shell.h

--- shell.h
+++ shell.h
@@ -12,17 +12,20 @@
 #include <fcntl.h>
 #include <stddef.h>
 extern char **environ;
-#define DELIMIT " \t\r\n\a"
-#define SIZE 1024
-#define PATH_MAX 1020
-int _getline();
-int _strlen(char *s);
-char _strdup(char *str);
-int _strcmp(char *s1, char *s2);
-int _strtok();
-void _fork();
-void sig_handler(int signum);
-volatile sig_atomic_t flag = 0;
-int _stat(int ac, char **av);
-int _putchar(char c);
+#define ARG_ONLY_SPACE 8
+
+char *read_line(char *line);
+void token_generate(char **args, char *line, char *delim);
+int execute(char **args);
+char *cmd(char *command);
+int _strlen(char *str);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+char *_strdup(const char *str);
+int _strcmp(char *dest, char *src);
+int builtin_cmd(char **args);
+void h_exit(char **args __attribute__((unused)));
+void h_cd(char **args);
+void h_help(char **args __attribute__((unused)));
+void h_env(char **args __attribute__((unused)));
 #endif
